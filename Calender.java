1a999d7c91a737a72b97ab30c0dbf5c77dc7c796
o_ob <74252011+weightprogrammer@users.noreply.github.com>
2020-12-27 19:01:27 +0900

    Add files via upload

--- Calender.java
+++ Calender.java
@@ -1,8 +1,11 @@
 package java_ex100;
 
+import java.util.Scanner;
+
 public class Calender {
 
 	public static void main(String[] args) {
+		
 		System.out.println("일  월   화  수   목   금  토");
 		System.out.println("--------------------------");
 		System.out.println("1   2   3   4   5   6   7");
@@ -10,7 +13,16 @@ public class Calender {
 		System.out.println("15  16  17  18  19  20  21");
 		System.out.println("22  23  24  25  26  27  28");
 		
-
+		
+		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 플로그램
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("달을 입력하세요");
+		int month = scanner.nextInt();
+		
+		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; 
+		
+		System.out.printf("%d월은 %d일까지 있습니다.\n", month, maxDays[month - 1]);
+		scanner.close();
 	}
 
 }
